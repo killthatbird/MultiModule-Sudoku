df3e052c1b30fea51548412e41639cbec5691193
uratamanabu <mabu_ip@msn.com>
2017-05-06 00:12:01 +0900

    messageの外部化を一部実施

--- micro-web/src/main/java/jp/co/valtech/sudoku/web/form/CreateForm.java
+++ micro-web/src/main/java/jp/co/valtech/sudoku/web/form/CreateForm.java
@@ -1,13 +1,12 @@
 package jp.co.valtech.sudoku.web.form;
 
+import java.io.Serializable;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Pattern;
 import jp.co.valtech.sudoku.core.form.BaseForm;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.hibernate.validator.constraints.NotBlank;
-
-import javax.validation.constraints.Digits;
-import javax.validation.constraints.Pattern;
-import java.io.Serializable;
 
 /**
  * @author uratamanabu
@@ -18,12 +17,12 @@ import java.io.Serializable;
 @EqualsAndHashCode(callSuper = false)
 public class CreateForm extends BaseForm implements Serializable {
 
-	private static final long serialVersionUID = -2616500675152025057L;
+		private static final long serialVersionUID = -2616500675152025057L;
 
-	@Digits(integer = 1, fraction = 0)
-	private int selectType;
+		@Digits(integer = 1, fraction = 0, message = "選択肢から選択してください。")
+		private int selectType;
 
-	@NotBlank
-	@Pattern(regexp = "easy|normal|hard")
-	private String selectLevel;
+		@NotBlank
+		@Pattern(regexp = "easy|normal|hard", message = "選択肢から選択してください。")
+		private String selectLevel;
 }
--- micro-web/src/main/java/jp/co/valtech/sudoku/web/form/DetailForm.java
+++ micro-web/src/main/java/jp/co/valtech/sudoku/web/form/DetailForm.java
@@ -1,14 +1,13 @@
 package jp.co.valtech.sudoku.web.form;
 
+import java.io.Serializable;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import jp.co.valtech.sudoku.core.form.BaseForm;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.hibernate.validator.constraints.NotBlank;
-
-import javax.validation.constraints.Digits;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
-import java.io.Serializable;
 
 /**
  * @author uratamanabu
@@ -19,16 +18,16 @@ import java.io.Serializable;
 @EqualsAndHashCode(callSuper = false)
 public class DetailForm extends BaseForm implements Serializable {
 
-	private static final long serialVersionUID = -2616500675152025057L;
+		private static final long serialVersionUID = -2616500675152025057L;
 
-	@Digits(integer = 1, fraction = 0)
-	private int type;
+		@Digits(integer = 1, fraction = 0, message = "選択肢から選択してください。")
+		private int type;
 
-	@NotBlank
-	@Pattern(regexp = "easy|normal|hard")
-	private String selectLevel;
+		@NotBlank
+		@Pattern(regexp = "easy|normal|hard", message = "選択肢から選択してください。")
+		private String selectLevel;
 
-	@NotBlank
-	@Size(max = 64)
-	private String keyHash;
+		@NotBlank
+		@Size(max = 64, message = "keyHashは64文字までです。")
+		private String keyHash;
 }
